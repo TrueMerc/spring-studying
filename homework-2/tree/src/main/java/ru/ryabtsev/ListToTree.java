8306b73cc65bc94ad9a505b069616e4f770ea785
TrueMerc <true_merc@mail.ru>
2019-05-06 10:55:50 +0300

    Add missed files.

---
+++ homework-2/tree/src/main/java/ru/ryabtsev/ListToTree.java
@@ -0,0 +1,13 @@
+package ru.ryabtsev;
+
+/**
+ * Hello world!
+ *
+ */
+public class ListToTree
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ homework-2/tree/src/test/java/ru/ryabtsev/AppTest.java
@@ -0,0 +1,20 @@
+package ru.ryabtsev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ListToTree.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
