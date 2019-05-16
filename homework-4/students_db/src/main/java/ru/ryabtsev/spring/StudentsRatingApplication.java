a00931ccdc6379be94ec4fa3123b6b135e2e44b6
TrueMerc <true_merc@mail.ru>
2019-05-16 05:32:24 +0300

    The last commit of the fourth homework.

--- homework-4/students_db/src/main/java/ru/ryabtsev/spring/StudentsRatingApplication.java
+++ homework-4/students_db/src/main/java/ru/ryabtsev/spring/StudentsRatingApplication.java
@@ -1,7 +1,12 @@
 package ru.ryabtsev.spring;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.net.URL;
+import java.security.ProtectionDomain;
 
 /**
  * Implements main application of the fourth homework of 'Spring Framework' course.
@@ -10,9 +15,11 @@ public class StudentsRatingApplication
 {
     public static void main( String[] args )
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("WEB-INF/config.xml");
         JdbcTemplateStudentDAO jdbc = context.getBean("JdbcTemplate", JdbcTemplateStudentDAO.class);
         Student student = new Student();
+
+// Database initialization is commited.
 //        student.setFirstName("John");
 //        student.setLastName("Walker");
 //        student.setId(5);
@@ -24,6 +31,28 @@ public class StudentsRatingApplication
             student = jdbc.findTitleById(i);
             if(null == student) { break; }
             System.out.println(student);
+        }
+
+
+        Server server = new Server(8189);
+
+        ProtectionDomain domain = StudentsRatingApplication.class.getProtectionDomain();
+        URL location = domain.getCodeSource().getLocation();
+
+        WebAppContext webapp = new WebAppContext();
+        webapp.setContextPath("/");
+        webapp.setWar(location.toExternalForm());
+
+        server.setHandler(webapp);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            server.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
