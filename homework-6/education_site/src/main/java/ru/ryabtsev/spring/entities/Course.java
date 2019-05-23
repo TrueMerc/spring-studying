b9d6db6e33ea95d49032ac54d77fccabf2ef63fe
TrueMerc <true_merc@mail.ru>
2019-05-23 10:31:08 +0300

    Add  html templates, Spring configuration files and Java application several files.

---
+++ homework-6/education_site/src/main/java/ru/ryabtsev/spring/entities/Course.java
@@ -0,0 +1,59 @@
+package ru.ryabtsev.spring.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "_courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "_process",
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students;
+
+    public Course() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [id = " + id + "] " + name + ": " + description;
+    }
+}
---
+++ homework-6/education_site/src/main/java/ru/ryabtsev/spring/entities/Student.java
@@ -0,0 +1,75 @@
+package ru.ryabtsev.spring.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="_students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToMany
+    @JoinTable(
+            name = "_process",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+    private List<Course> courses;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Student() {
+
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Student [id = " + id + "] " + lastName + " " + firstName + "\nCourses list:\n";
+        for(Course course : courses) {
+            result += course;
+        }
+        return result;
+    }
+
+}
