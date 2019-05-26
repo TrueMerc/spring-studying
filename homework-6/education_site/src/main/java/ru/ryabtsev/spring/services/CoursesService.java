2848fb599ffb93f3fca4112b661240654a46b483
TrueMerc <true_merc@mail.ru>
2019-05-26 12:51:22 +0300

    Commit last changes.

---
+++ homework-6/education_site/src/main/java/ru/ryabtsev/spring/services/CoursesService.java
@@ -0,0 +1,9 @@
+package ru.ryabtsev.spring.services;
+
+import org.springframework.stereotype.Service;
+import ru.ryabtsev.spring.repositories.CoursesRepository;
+
+@Service
+public class CoursesService {
+    private CoursesRepository coursesRepository;
+}
