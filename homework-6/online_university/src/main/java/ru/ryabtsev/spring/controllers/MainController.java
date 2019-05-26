fa173a2312497729afda335847431b09412c4701
TrueMerc <true_merc@mail.ru>
2019-05-26 19:26:52 +0300

    Add main page controller.

---
+++ homework-6/online_university/src/main/java/ru/ryabtsev/spring/controllers/MainController.java
@@ -0,0 +1,23 @@
+package ru.ryabtsev.spring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Implements main page controller.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
+//    @Secured({"ROLE_ADMIN"})
+//    @RequestMapping("/administratorOnly")
+//    public String administratorOnly() {
+//        return "index";
+//    }
+}
